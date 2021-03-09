Working AE; Acc > 91% on validation; Fixed bug of accuracy calculation

HW3/AE_Rec_Sys.py
@@ -26,7 +26,6 @@ class AutoEncoderCustomDataset(Dataset):
     Define x, y for data of AutoEncoder
     The y is x (for reconstruction error)
     """
-
     def __init__(self, features: np.array):
         self.features = torch.Tensor(features).float()
         self.labels = torch.Tensor(features).float()
@@ -41,11 +40,11 @@ class AutoEncoderCustomDataset(Dataset):
 class AutoEncoder(nn.Module):
     def __init__(self, input_dim: int, output_dims: List[int]):
         super(AutoEncoder, self).__init__()
-        encoder_layers = [nn.Linear(input_dim, output_dims[0])]
+        encoder_layers = [nn.Dropout(0.5), nn.Linear(input_dim, output_dims[0])]
         decoder_layers = []
         for i in range(1, len(output_dims)):
             encoder_layers.append(nn.ReLU())
-            encoder_layers.append(nn.Linear(output_dims[i - 1], output_dims[i]))
+            encoder_layers.extend([nn.Linear(output_dims[i - 1], output_dims[i]), nn.Sigmoid()])
         self.encoder = nn.Sequential(*encoder_layers)
 
         for i in range(1, len(output_dims)):
@@ -58,10 +57,6 @@ class AutoEncoder(nn.Module):
         encoding = self.encoder(x)
         reconstruction = self.decoder(encoding)
         return reconstruction
-
-    @staticmethod
-    def get_loss(original, reconstruction):
-        F.l1_loss(input=reconstruction, target=original)
 
 
 def focal_loss(input, target,alpha=ALPHA_TENSOR, gamma: float = GAMMA) -> Tensor:
@@ -111,17 +106,18 @@ def train(net: nn.Module, optimizer: torch.optim, train_dataloader: DataLoader =
 
         net.train()
         for x_train, y_train in tqdm(train_dataloader):
+            # weights = torch.where(y_train.flatten() == 0, torch.tensor(0.1), torch.tensor(0.9))
+            # loss_fn = nn.BCEWithLogitsLoss(pos_weight=weights)
             if train_on_gpu:
                 net.cuda()
                 x_train, y_train = x_train.cuda(), y_train.cuda()
             optimizer.zero_grad()
             y_train_pred = net(x_train)
 
-            loss = loss_fn(y_train_pred, y_train)
+            loss = loss_fn(y_train_pred.flatten(), y_train.flatten())
             loss_running += loss.item()
             loss.backward()
             optimizer.step()
-            # classfied_data = classify_preference_for_each_user(infer_df=validation_rand_df, y_pred=y_train_pred)
             train_preds = np.where(y_train_pred > 0.5, 1, 0)
             train_correct_counter += (train_preds == np.array(y_train)).sum()
             train_positive_number += get_number_of_positves(y=y_train)
@@ -172,7 +168,7 @@ def train(net: nn.Module, optimizer: torch.optim, train_dataloader: DataLoader =
     return net
 
 
-def infer(net: nn.Module, infer_dataloader: DataLoader, loss_fn: loss) -> Tuple[float, float, float]:
+def infer(net: nn.Module, infer_dataloader: DataLoader, loss_fn: loss=None) -> Tuple[float, float]:
     """
     Run the model on x_infer (both validation and test) and calculate the loss of the predictions.
     The model run on evaluation mode and without updating the computational graph (no_grad)
@@ -187,19 +183,19 @@ def infer(net: nn.Module, infer_dataloader: DataLoader, loss_fn: loss) -> Tuple[
     infer_auc_accumulated = 0
 
     for x, y in infer_dataloader:
+        # weights = torch.where(y.flatten() == 0, torch.tensor(0.1), torch.tensor(0.9))
+        # loss_fn = nn.BCEWithLogitsLoss(pos_weight=weights)
         with torch.no_grad():
             if train_on_gpu:
                 x, y = x.cuda(), y.cuda()
             y_pred = net(x)
+            y_true = get_classification_bit_gt(infer_df=validation_rand_df)
             infer_preds = classify_preference_for_each_user(infer_df=validation_rand_df, y_pred=y_pred)
-            infer_correct_counter += len(infer_preds) - infer_preds.sum() # All the validation correct preds located in index 0
-            # infer_auc_accumulated += calculate_auc_score(y_true=y, y_pred=preds)
-            infer_loss = loss_fn(y_pred, y)
+            infer_correct_counter += (y_true == infer_preds).sum()
+            infer_loss = loss_fn(y_pred.flatten(), y.flatten())
 
         running_loss += infer_loss
-    # infer_correct_counter = infer_correct_counter.item()
-    return running_loss / len(infer_dataloader), infer_correct_counter / len(
-        infer_dataloader.dataset)  # , infer_auc_accumulated / len(infer_dataloader)
+    return running_loss / len(infer_dataloader), infer_correct_counter / len(infer_dataloader.dataset)  # , infer_auc_accumulated / len(infer_dataloader)
 
 
 if __name__ == '__main__':
@@ -209,13 +205,17 @@ if __name__ == '__main__':
     train_dataloader = DataLoader(AutoEncoderCustomDataset(train_preferences_matrix), batch_size=BATCH_SIZE)
     val_dataloader = DataLoader(AutoEncoderCustomDataset(train_preferences_matrix), batch_size=NUM_USERS)
     user_vector_size = train_preferences_matrix.shape[1]
-    # loss_fn = nn.BCELoss()
-    loss_fn = FocalLoss(alpha=ALPHA_TENSOR, gamma=GAMMA)
+    # loss_fn = nn.MSELoss()
+    loss_fn = nn.BCELoss()
+    # loss_fn = nn.BCEWithLogitsLoss()
+    # loss_fn = FocalLoss(alpha=ALPHA_TENSOR, gamma=GAMMA)
     # loss_fn = focal_loss
-    ae = AutoEncoder(input_dim=user_vector_size, output_dims=[2048, 1024, 512, 256, 128, 64])
+    ae = AutoEncoder(input_dim=user_vector_size, output_dims=[250])
     # print_trainable_params(ae)
 
-    optimizer = torch.optim.Adam(ae.parameters(), lr=lr)  # , weight_decay=WEIGHT_DECAY)
+    optimizer = torch.optim.Adam(ae.parameters(), lr=lr, weight_decay=WEIGHT_DECAY)
     net = train(net=ae, optimizer=optimizer, train_dataloader=train_dataloader,
                 val_dataloader=val_dataloader, is_earlystopping=True)
+    # print('End of Training - Infer mode:')
+    # infer(net, val_dataloader, loss_fn)
     save_pt_model(net=net)

HW3/utils.py
@@ -63,11 +63,12 @@ def get_mask_matrix_by_infer_df(infer_df: pd.DataFrame, y_pred_shape: Tuple[int,
     For each user there are 2 items concatenated to users_items.
     :return: mask array (ma)
     """
-    users_items = [[item_1, item_2] for item_1, item_2 in zip(infer_df[:, 1], infer_df[:, 2])]
+    users_items = [[item_1 - 1, item_2 - 1] for item_1, item_2 in zip(infer_df[:, 1], infer_df[:, 2])]
     ma = np.zeros(y_pred_shape)
     for user_id, user_items in zip(infer_df[:, 0], users_items):
-        np.put(ma[user_id], user_items, 1)
+        np.put(ma[user_id - 1], user_items, 1)
     return ma
+
 
 def get_user_items_scores_by_mask_matrix(y_pred_scores: np.array, mask_matrix: np.array) -> np.array:
     """
@@ -88,9 +89,18 @@ def classify_preference_for_each_user(infer_df: pd.DataFrame, y_pred: np.array) 
     :return: Index of the most likely item for each user
     """
     mask_matrix = get_mask_matrix_by_infer_df(infer_df=infer_df, y_pred_shape=y_pred.shape)
-    user_items_scores = get_user_items_scores_by_mask_matrix(mask_matrix=mask_matrix,
-                                                             y_pred_scores=y_pred)
+    user_items_scores = get_user_items_scores_by_mask_matrix(mask_matrix=mask_matrix, y_pred_scores=y_pred)
     return np.argmax(user_items_scores, axis=1)
+
+
+def get_classification_bit_gt(infer_df: np.array) -> np.array:
+    """
+    As the mask in classify_preference_for_each_user make the probabilities order ascending (the first probability belongs
+    to the smaller item), we need to custom the bit classification - the index of the correct item is the result of the condition
+    of after max the item values against the location of the first item (in the validation this is the correct)
+    :return: numpy array of classification bits
+    """
+    return (infer_df[:, 1] == np.max(infer_df[:, 1:3], axis=1)).astype(int)
 
 
 def calculate_model_metrics(y_true: np.array, y_pred: np.array, verbose: bool = True, mode: str = 'Test') -> Tuple[
@@ -221,28 +231,33 @@ def print_trainable_params(net: nn.Module) -> None:
             print('\t', name)
 
 
-def get_num_users_items(df: pd.DataFrame) -> Tuple[int, int]:
-    return df.UserID.max() + 1, df.ItemID.max() + 1
+def get_num_users_items(df) -> Tuple[int, int]:
+    if type(df) == pd.DataFrame:
+        df = df.to_numpy()
+    return len(np.unique(df[:, 0])), len(np.unique(df[:, 1])) + 1
 
 
-def get_train_random_validation_data(train_df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+def get_train_random_validation_data(train_df: pd.DataFrame) -> Tuple[np.array, np.array]:
     """
     Creating random validation set from the train set - for each user, there is only one record in the validation
     The item the user liked is in the first column ("Item1")
     """
+    train_df = train_df.to_numpy()
     num_users, num_items = get_num_users_items(df=train_df)
     validation_indices = []
-    validation_df = pd.DataFrame(columns=['UserID', 'Item1', 'Item2'])
-    for user_id in train_df.UserID.values:
-        user_data = train_df.query('UserID == @user_id')
-        validation_row = user_data.sample(1)
-        validation_row.rename(columns={'ItemID': 'Item1'}, inplace=True)
-        negative_item = np.random.choice(list(set(range(1, num_items)).difference(user_data.ItemID.unique())), 1)[0]
-        validation_row['Item2'] = negative_item
-        validation_indices.append(validation_row.index[0])
-        validation_df = validation_df.append(validation_row)
-    train_df.drop(validation_indices, inplace=True)
-    train_df.reset_index(drop=True, inplace=True)
+    validation_df = np.zeros((num_users, 3))
+    for user_id in np.unique(train_df[:, 0]):
+        user_items = np.where(train_df[:, 0] == user_id)[0]
+        validation_user_item_index = np.random.choice(user_items)
+        validation_user_item = train_df[validation_user_item_index][1]
+        negative_user_item = np.random.choice(list(set(range(1, num_items)).difference(user_items)), 1)[0]
+
+        validation_df[user_id - 1][0] = user_id
+        validation_df[user_id - 1][1] = validation_user_item
+        validation_df[user_id - 1][2] = negative_user_item
+        validation_indices.append(validation_user_item_index)
+
+    train_df = np.delete(train_df, validation_indices, axis=0)
     return train_df, validation_df.astype(np.int64)
 
 
@@ -254,8 +269,8 @@ def create_user_items_preferences_matrix(df: pd.DataFrame) -> np.array:
     """
     fill_value = 1
     preferences_matrix = np.zeros(get_num_users_items(df=df))
-    for user_id in df.UserID.values:
-        np.put(preferences_matrix[user_id], df.query('UserID == @user_id').ItemID.values, fill_value)
+    for user_id in np.unique(df[:, 0]):
+        np.put(preferences_matrix[user_id - 1], df[np.where(df[:, 0] == user_id)][:, 1] - 1, fill_value)
     return preferences_matrix.astype(np.float64)
 
 
@@ -273,7 +288,6 @@ def get_validation_and_train_matrix(train_df: pd.DataFrame) -> Tuple[pd.DataFram
     """
     Extracting validation dataset from the train set, saving object for saving time while debugging
     """
-
     if len(os.listdir((Path.cwd() / "hdf5"))) == 0:
         train_df, validation_rand_df = get_train_random_validation_data(train_df=train_df)
         train_preferences_matrix = create_user_items_preferences_matrix(df=train_df)